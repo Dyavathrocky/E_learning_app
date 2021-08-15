8348c1a093111e70678636ba2e0dea025eeb2d74
rakesh <davathrak@gmail.com>
2021-08-15 09:58:43 +0530

    added custom fileds and content types

--- edu/courses/models.py
+++ edu/courses/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
+from .fields import OrderField
+
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
@@ -16,8 +21,8 @@ class Subject(models.Model):
 
 
 class Course(models.Model):
-    owner = models.ForeignKey(User, related_name='courses_created', on_delete=CASCADE)
-    subject = models.ForeignKey(Subject, related_name='courses', on_delete=CASCADE)
+    owner = models.ForeignKey(User, related_name='courses_created', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
@@ -31,10 +36,56 @@ class Course(models.Model):
 
 
 class Module(models.Model):
-    course = models.ForeignKey(Course, related_name='modules',on_delete=CASCADE)
+    course = models.ForeignKey(Course, related_name='modules',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True , for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
+        return f'{self.order}. {self.title}'
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType , on_delete=models.CASCADE,
+                                    limit_choices_to={'model__in':(
+                                        'text',
+                                        'video',
+                                        'image',
+                                        'file'
+                                    )})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+    
+    class Meta:
+        ordering = ['order']
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
         return self.title
+
+
+class Text(ItemBase):
+    text = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
     
